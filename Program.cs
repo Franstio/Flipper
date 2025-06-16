1cc217a59ce9b97228c9f8e833e963c00954994c
admindpe-dharma <admindpe@dharmapacific.com>
2025-06-16 16:33:20 +0700

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using FVMI_INSPECTION.TCP;
+
 namespace FVMI_INSPECTION
 {
     internal static class Program
@@ -10,6 +12,8 @@ namespace FVMI_INSPECTION
          {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            FVMITcpClient process = new FVMITcpClient();
+            process.WriteCommand("MR2100", 0).RunSynchronously();
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm() { WindowState= FormWindowState.Maximized} );
         }
