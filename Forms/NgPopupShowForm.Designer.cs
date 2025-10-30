26c44167d8d8d508ea229f6ca41a68053be1d524
Franstio <franstiodiego01@gmail.com>
2025-10-30 14:46:23 +0700

    OK

--- Forms/NgPopupShowForm.Designer.cs
+++ Forms/NgPopupShowForm.Designer.cs
@@ -94,6 +94,7 @@ namespace FVMI_INSPECTION.Forms
             actualPictureBox.MovPos = new Point(0, 0);
             actualPictureBox.Name = "actualPictureBox";
             actualPictureBox.Size = new Size(453, 447);
+            actualPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
             actualPictureBox.StartPos = new Point(0, 0);
             actualPictureBox.TabIndex = 2;
             actualPictureBox.TabStop = false;
@@ -162,6 +163,7 @@ namespace FVMI_INSPECTION.Forms
             parameterPictureBox.MovPos = new Point(0, 0);
             parameterPictureBox.Name = "parameterPictureBox";
             parameterPictureBox.Size = new Size(453, 447);
+            parameterPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
             parameterPictureBox.StartPos = new Point(0, 0);
             parameterPictureBox.TabIndex = 2;
             parameterPictureBox.TabStop = false;
